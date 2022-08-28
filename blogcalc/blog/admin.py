2bcd0611dab9b1cd77f51ab329d03f5ac2cd8eba
Tren-123 <pavel.filatov1@yandex.ru>
2022-08-28 22:48:24 +0300

    Added model Bloger, installed python package Pillow for image storage in db, changed UML table

--- blogcalc/blog/admin.py
+++ blogcalc/blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import TestModel
+from .models import TestModel, Blogger
 
 # Register your models here.
 
 
 
 admin.site.register(TestModel)
+admin.site.register(Blogger)
