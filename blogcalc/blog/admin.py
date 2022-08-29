deb8bab4026dce8782317bc124e20ba1be11b3f6
Tren-123 <pavel.filatov1@yandex.ru>
2022-08-29 13:08:59 +0300

    Added Blog_post model

--- blogcalc/blog/admin.py
+++ blogcalc/blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Blogger
+from .models import Blogger, Blog_post
 
 # Register your models here.
 
 
 admin.site.register(Blogger)
+admin.site.register(Blog_post)
--- blogcalc/blog/models.py
+++ blogcalc/blog/models.py
@@ -1,15 +1,17 @@
 from distutils.command.upload import upload
+from email.utils import localtime
 from django.db import models
-from datetime import date
+import time
+from datetime import datetime
 
 # Create your models here.
 
 class Blogger(models.Model):
-    """Model for blogger info storage"""
+    """Model representing a blogger info"""
     first_name = models.CharField(max_length=50, blank=True, null=True, help_text='Enter your first name')
     second_name = models.CharField(max_length=50, blank=True, null=True, help_text='Enter your second name')
     nickname = models.CharField(max_length=50, unique=True, blank=False, help_text='Enter unique nickname')
-    e_mail = models.EmailField("E-mail", unique=True, blank=False, help_text='Enter your e-mail. Each e-mail can be registred only one time')
+    e_mail = models.EmailField("E-mail", unique=True, blank=False, help_text="Enter your e-mail. Each e-mail can be registred only one time")
     date_of_bitrh = models.DateField(blank=True, null=True, help_text="Enter your date of birth")
     short_bio = models.TextField(max_length=1000, blank=True, null=True, help_text="Enter your short bio")
     #Avatar field not completed. Should set sizes for storage images and directory for storage images.
@@ -19,3 +21,16 @@ class Blogger(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return self.nickname
+
+class Blog_post(models.Model):
+    """Model representing blog posts related for bloggers"""
+    title = models.CharField(max_length=100, help_text="Enter a title that briefly reflects the essence of the post")
+    content = models.TextField(help_text="Enter your text here")
+    likes = models.PositiveIntegerField(default=0)
+    dislikes = models.PositiveIntegerField(default=0)
+    date_of_origin = datetime(*time.localtime()[0:6]) 
+    blogger = models.ForeignKey(Blogger, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f"{self.title}, {self.date_of_origin}"
