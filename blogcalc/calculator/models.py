7fd7b2055598f4b045e72461da58fcd4ae8d2066
Dmitry Oleynikov <dmitry.oleynikov1@gmail.com>
2022-09-05 19:19:21 +0200

    simple visualization added

--- blogcalc/calculator/models.py
+++ blogcalc/calculator/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Calculator(models.Model):
-    title = models.CharField(max_length=200)
-    context = models.CharField(max_length=1500)
+class Visualizer(models.Model):
+    product_category = models.CharField(max_length=20)
+    payment_method = models.CharField(max_length=50)
+    shipping_cost = models.CharField(max_length=50)
+    unit_price = models.DecimalField(max_digits=5, decimal_places=2)
 
 
     def __str__(self):
--- blogcalc/calculator/urls.py
+++ blogcalc/calculator/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='calculator-index'),
+    path('', views.index, name='visualizer-index'),
+    path('data', views.pivot_data, name='pivot_data')
 ]
