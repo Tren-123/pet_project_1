2f933a9a39a8d879131524813be4c18c611697e0
Tren-123 <pavel.filatov1@yandex.ru>
2022-09-27 11:14:15 +0300

    s

--- blogcalc/blogcalc/wsgi.py
+++ blogcalc/blogcalc/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogcalc.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogcalc.blogcalc.settings')
 
 application = get_wsgi_application()
